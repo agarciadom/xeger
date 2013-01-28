06da3c2ab510109dc8d43003ef859430f45cbf18
Antonio Garcia-Dominguez <antonio.garciadominguez@uca.es>
2013-01-28 20:57:52 +0100

    Add copyright lines for bugfix authors - header must be the same in all source files

--- src/test/java/nl/flotsam/xeger/XegerUtilsTest.java
+++ src/test/java/nl/flotsam/xeger/XegerUtilsTest.java
@@ -1,5 +1,7 @@
 /**
  * Copyright 2009 Wilfred Springer
+ * Copyright 2012 Jason Pell
+ * Copyright 2013 Antonio García-Domínguez
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
